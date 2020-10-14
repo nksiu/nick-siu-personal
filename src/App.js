1d48045a30b1f71436e55876f53804cb2f0f912b
Nicholas Siu <ncyk.siu@gmail.com>
2020-10-14 15:33:39 -0700

    establish routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="container">
-        <p>hi</p>
-    </div>
-  );
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+// Components
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Router>
+            <Header />
+            <Route exact path="/" render={() => <Home />} />
+            <Route exact path="/about" render={() => <About />} />
+          </Router>
+      </div>
+    );
+  };
 }
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {Link} from "react-router-dom"
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <Link to={"/"} className="navbar-brand">Nick Siu</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarColor01">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <Link to={"/"} className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/about"} className="nav-link">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
