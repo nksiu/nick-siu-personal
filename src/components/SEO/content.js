871d1f2f3fe91c3d70fe1d8b62011f6e5b3b4f8b
nksiu <46730884+nksiu@users.noreply.github.com>
2020-11-11 16:55:06 -0800

    react-helmet and meta content (#3)
    Co-authored-by: Nicholas Siu <ncyk.siu@gmail.com>

---
+++ src/components/SEO/content.js
@@ -0,0 +1,28 @@
+export const seoContents = {
+  "meta": [
+    {
+      page: "default",
+      title: "Nick Siu's Personal Website",
+      description: "Portfolio and Work Experience of a Software Engineer, QA Engineer, and Student",
+      keywords: "portfolio, software, nick siu"
+    },
+    {
+      page: "about",
+      title: "Nick Siu | About",
+      description: "Introduction to Nick Siu",
+      keywords: "nick siu, about, Computer Science, University of British Columbia"
+    },
+    {
+      page: "experience",
+      title: "Nick Siu | Experience",
+      description: "Work Experience of Nick Siu",
+      keywords: "Nick Siu, Work Experience, Computer Science, Software Engineer, QA Engineer"
+    },
+    {
+      page: "portfolio",
+      title: "Nick Siu | Portfolio",
+      description: "Portfolio of Nick Siu",
+      keywords: "Portfolio, Github, Nick Siu, Calendar, Spotify"
+    },
+  ]
+}
